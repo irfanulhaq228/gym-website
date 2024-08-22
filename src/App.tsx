02cef177e44f07a6705414a99935db013b1056ff
IRFAN UL HAQ <irfanulhaq228@gmail.com>
2024-08-22 14:32:15 +0500

    version 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import "./App.css";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Login from "./pages/auth/login";
 import Signup from "./pages/auth/signup";
-import { useEffect } from "react";
+
+import Home from "./pages/home/home";
 
 function App() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/signup");
-  }, [navigate]);
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/signup");
+  }, [navigate]);
+  return <div>Home</div>;
+};
+
+export default Home;
